435df2acc459bda478b33d7d5467c7ea0fdf94cc
Ruben Dorado <ruben.dorados@gmail.com>
2018-11-02 00:09:35 -0400

    First commit

---
+++ src/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="news-inspector",
+    version="0.0.1",
+    author="Ruben Dorado",
+    author_email="ruben.dorados@gmail.com",
+    description="Utilities to analyze and extract information from news",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rdorado/news-inspector",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
