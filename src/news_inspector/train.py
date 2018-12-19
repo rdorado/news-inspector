477a76685a4f5686665d7c369b9a42bb6097b3e1
Ruben Dorado <ruben.dorados@gmail.com>
2018-12-19 15:55:17 -0500

    Update train.py

--- src/news_inspector/train.py
+++ src/news_inspector/train.py
@@ -1,7 +1,12 @@
 
+class TrainingConfiguration:
+    pass 
 
+def load_config(config_file) {
+    return null;
+}
 
+def classifier_trainer(class, config, output) {
+    config = load_config(config);
+}
 
-def train():
-    pass
-  
