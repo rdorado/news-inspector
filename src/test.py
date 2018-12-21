22b58991aa7de5eba87f4c3bf99de79be09e8590
Ruben Dorado <ruben.dorados@gmail.com>
2018-12-21 10:57:26 -0500

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,21 +1,22 @@
-from news_inspector.core import train_classifier
+from news_inspector.core import train_model
 from news_inspector.core import load_model
+from news_inspector import nlp
 
 from news_inspector.classifiers import PolarityClassifier
 
 
-train_classifier(PolarityClassifier, "polarity-classifier-training-config.xml", "polarity-classifier.v0.model");
+train_model(PolarityClassifier, "polarity-classifier-training-config.xml", "polarity-classifier.v0.model");
 model = load_model("polarity-classifier.v0.model");
 result = model.classify(text);
 
 
-train_retriever(LocationRetriever, "location-ir-training-config.xml", "location-ir.v0.model");
+train_model(LocationRetriever, "location-ir-training-config.xml", "location-ir.v0.model");
 model = load_model("location-ir.v0.model");
 locations = model.retrieve(text);
 
 keywords = nlp.getKeywords(text);
 
 
-train_knowledgebase(NaiveKBManager, "naive-knowledgebase-training-config.xml", "naive-knowledgebase.v0.model");
+train_model(NaiveKBManager, "naive-knowledgebase-training-config.xml", "naive-knowledgebase.v0.model");
 model = load_model("naive-knowledgebase.v0.model");
 graph = model.adjust(locations + keywords);
