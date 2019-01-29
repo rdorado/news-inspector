14dea0ae2cd90fc1fe7e2bbd4e52ca5c9ec929f5
Ruben Dorado <ruben.dorados@gmail.com>
2019-01-29 09:11:03 -0500

    Implemented first version of the information retriever

--- src/news_inspector/nlp.py
+++ src/news_inspector/nlp.py
@@ -7,8 +7,8 @@ class DefaultTextProcessor:
 
 def getKeywords(text):
     resp = []
-    for sentence in nltk.tokenize.sent_tokenize(text):
-       print("\n"+sentence)
+    for sentence in nltk.tokenize.sent_tokenize(text):       
        sentence = nltk.pos_tag(nltk.word_tokenize(sentence.lower()))
+       print("\n"+sentence)
        resp.append(sentence)
     return resp
