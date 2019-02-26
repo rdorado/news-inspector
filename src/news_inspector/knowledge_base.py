2bda6dba8c8f9020c2cc9579750b12ed4d4beff4
Ruben Dorado <ruben.dorados@gmail.com>
2019-02-26 17:34:09 -0500

    Update knowledge_base.py

--- src/news_inspector/knowledge_base.py
+++ src/news_inspector/knowledge_base.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+import xml.etree.ElementTree as xmlreader
 
 from news_inspector.core import Trainable
 
@@ -8,25 +9,67 @@ class KnowledgeBase(Trainable):
     def makeGraph(self, array):
         pass    
 
-
-class Node:
+    def loadNode(self, nodename, params=None): 
+        filename = self.repodir+"/"+nodename
+        if not os.path.isfile(filename):
+            raise Exception("Fatal error. Training configuration file '"+filename+"' not found.")
     
-    def __init__(self):
+        tree = xmlreader.parse(filename)
+        root = tree.getroot()         
+    
+        module = root.attrib['module']
+        class_name = root.attrib['class']
+        clazz = getattr(module, class_name)
+        
+        node = clazz(root.attrib)
+        node.id=int(root.attrib['id'])
+        node.name=root.attrib['name']
+        
+        for doc in root.iter('arc'):
+            module = doc.attrib['module']
+            class_name = doc.attrib['class']
+            clazz = getattr(module, class_name)
+            node.addArc(clazz(doc.attrib))
+        
+        self.nodeids[node.id] = node
+        self.nodenames[node.name] = node.id    
+    
+    
+class Node(ABC):
+     
+    def __init__(self, attribs):    
+        self.id = attribs['id']
+        self.name = attribs['name']
+    
+    @abstractmethod    
+    def save(repodir):    
+        pass    
+    
+    
+    
+class Arc(ABC):
+
+    def __init__(self, attribs):
+        pass
+            
+    
+class SimpleNode(Node):   
+    
+    def __init__(self, attribs):    
         self.id = None
         self.name = None
         self.arcs = {}  # 'type_of_rel': [id1, id2] 
-                
-
-        
+   
     def save(repodir):    
         pass
     
-class Arc:
+    
+class WeightedArc(Arc):
 
-    def __init__(self):
-        self.target = None
-        self.weight = 1
-        self.time = 0
+    def __init__(self, attribs):
+        self.target = int(attribs['target'])
+        self.weight = float(attribs['weight'])
+        self.time = int(attribs['time'])
         
         
     
@@ -43,3 +86,6 @@ class NaiveKnowledgeBase(KnowledgeBase):
 
     def makeGraph(self, array):
         return KBGraph() 
+    
+
+        
