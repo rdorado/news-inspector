11407ef3d90963e3731587b83325e26498c57a52
Ruben Dorado <ruben.dorados@gmail.com>
2019-02-26 14:51:41 -0500

    Update knowledge_base.py

--- src/news_inspector/knowledge_base.py
+++ src/news_inspector/knowledge_base.py
@@ -9,6 +9,27 @@ class KnowledgeBase(Trainable):
         pass    
 
 
+class Node:
+    
+    def __init__(self):
+        self.id = None
+        self.name = None
+        self.arcs = {}  # 'type_of_rel': [id1, id2] 
+                
+
+        
+    def save(repodir):    
+        pass
+    
+class Arc:
+
+    def __init__(self):
+        self.target = None
+        self.weight = 1
+        self.time = 0
+        
+        
+    
 class KBGraph:
     
     def __init__(self):
