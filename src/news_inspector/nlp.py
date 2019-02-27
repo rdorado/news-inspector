05e48bc632d13303fa3236d3f3b3ad007bdc7e6b
Ruben Dorado <ruben.dorados@gmail.com>
2019-02-27 11:55:37 -0500

    Update nlp.py

--- src/news_inspector/nlp.py
+++ src/news_inspector/nlp.py
@@ -3,22 +3,32 @@ from nltk.tokenize import RegexpTokenizer, word_tokenize, sent_tokenize
 from sklearn.feature_extraction import stop_words
 #from nltk.corpus import stopwords
 
+def getKeywords(text):
+    resp = []
+    for sentence in sent_tokenize(text):
+        tokenizer = RegexpTokenizer(r'[a-z]+')
+        sentence = sentence.lower()        
+        #tokens = word_tokenize(sentence)
+        tokens = tokenizer.tokenize(sentence)  
+        print(tokens) 
+        #words = [w for w in tokens if not w in stopwords.words('english')]
+        words = [w for w in tokens if not w in stop_words.ENGLISH_STOP_WORDS]
+
+        sentence = nltk.pos_tag(words)
+        resp.append(sentence)
+    return resp
+
+def getSentences(text):
+    return sent_tokenize(text): 
+
+def getWords(text):
+    tokenizer = RegexpTokenizer(r'[a-z]+')
+    tokens = tokenizer.tokenize(text)  
+    return [w for w in tokens if not w in stop_words.ENGLISH_STOP_WORDS]
+    
 class DefaultTextProcessor:
     
     def process(self, text):
         return text
 
-def getKeywords(text):
-    resp = []
-    for sentence in sent_tokenize(text):
-       tokenizer = RegexpTokenizer(r'[a-z]+')
-       sentence = sentence.lower()        
-       #tokens = word_tokenize(sentence)
-       tokens = tokenizer.tokenize(sentence)  
-       print(tokens) 
-       #words = [w for w in tokens if not w in stopwords.words('english')]
-       words = [w for w in tokens if not w in stop_words.ENGLISH_STOP_WORDS]
 
-       sentence = nltk.pos_tag(words)
-       resp.append(sentence)
-    return resp
